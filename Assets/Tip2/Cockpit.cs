e0dc8e7d3ec3cbbdb962ca247be905ba68d10847
DeveloperHan <hangksgogo@gmail.com>
2021-01-31 02:56:54 +0900

    Revert "Homework"
    This reverts commit 5bfc7bd518f168a0cc6c4298a0fe5545d9f90f14.

--- Assets/Tip2/Cockpit.cs
+++ Assets/Tip2/Cockpit.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace ReferenceAndEventDemo
@@ -8,33 +8,8 @@ namespace ReferenceAndEventDemo
         [SerializeField] private Button toggleButton = null;
         [SerializeField] private Text stateText = null;
 
-        private Airplane airplane;
-
-        private void Start()
+        void Awake()
         {
-            airplane = GetComponentInParent<Airplane>();
-            airplane.OnDetectEngineFlawEvent += HandleDetectEngineFlawEvent;
-            airplane.OnEnginesReadyEvent += HandleEnginesReadyEvent;
-            airplane.OnEnginesOffEvent += HandleEnginesOffEvent;
-        }
-        private void HandleDetectEngineFlawEvent()
-        {
-            ChangeStateText("엔진 ERROR");
-        }
-
-        private void HandleEnginesReadyEvent()
-        {
-            ChangeStateText("엔진 ON");
-        }
-
-        private void HandleEnginesOffEvent()
-        {
-            ChangeStateText("엔진 OFF");
-        }
-
-        public void ChangeStateText(string _state)
-        {
-            stateText.text = _state;
         }
     }
 }
--- Assets/Tip2/ToggleButton.cs
+++ Assets/Tip2/ToggleButton.cs
@@ -1,86 +1,10 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace ReferenceAndEventDemo
 {
     public class ToggleButton : MonoBehaviour
     {
-        public enum ToggleCommand { EngineOn, EngineOff, EngineReset }
-        private ToggleCommand toggleCommand = ToggleCommand.EngineOn;
         [SerializeField] private Text text = null;
-
-        private Airplane airplane;
-
-        private void Start()
-        {
-            airplane = GetComponentInParent<Airplane>();
-            airplane.OnDetectEngineFlawEvent += HandleDetectEngineFlawEvent;
-            airplane.OnEnginesReadyEvent += HandleEngineReadyEvent;
-            airplane.OnEnginesOffEvent += HandleEnginesOff;
-        }
-
-        public void ToggleButtonEvent()
-        {
-            switch (toggleCommand)
-            {
-                case ToggleCommand.EngineOn:
-                    airplane.StartEngineCheck();
-                    break;
-
-                case ToggleCommand.EngineOff:
-                    airplane.TurnOffEngines();
-                    break;
-
-                case ToggleCommand.EngineReset:
-                    airplane.ResetEngines();
-                    break;
-            }
-
-        }
-
-        private void HandleDetectEngineFlawEvent()
-        {
-            SwitchToggleCommand(ToggleCommand.EngineReset);
-
-        }
-
-        private void HandleEngineReadyEvent()
-        {
-            SwitchToggleCommand(ToggleCommand.EngineOff);
-        }
-
-        private void HandleEnginesOff()
-        {
-            SwitchToggleCommand(ToggleCommand.EngineOn);
-        }
-
-        public void SwitchToggleCommand(ToggleCommand command)
-        {
-            toggleCommand = command;
-
-            switch (toggleCommand)
-            {
-                case ToggleCommand.EngineOn:
-                    text.text = "엔진 ON";
-                    break;
-
-                case ToggleCommand.EngineOff:
-                    text.text = "엔진 OFF";
-                    break;
-
-                case ToggleCommand.EngineReset:
-                    text.text = "엔진 RESET";
-                    break;
-                default:
-                    text.text = "";
-                    break;
-            }
-        }
-
-
-
-
-
     }
-
 }
